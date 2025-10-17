cdcc904fb461875893aacee2a14dfbe009a5f5df
Alexander Kim <avxkim@gmail.com>
2025-10-17 12:55:28 +0500

    chore: clear

--- test-hook.ts
+++
@@ -1,5 +0,0 @@
-const broken={a:1,b:2,c:3,d:4};function test(x:number){return x*2}const data=[1,2,3];
-
-interface Broken{foo:string;bar:number;baz:boolean}
-
-class BrokenClass{constructor(private val:number){}method(){return this.val*2}}
